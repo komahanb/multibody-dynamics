c02da10e591f7427d119992eed3281789f473c33
komahan <komahan.cool@gmail.com>
2016-04-14 16:49:04 -0400

    cleaned up code

--- src/main.f90
+++ src/main.f90
@@ -9,35 +9,14 @@ program test
   use rigid_body_dynamics_class, only: rigid_body_dynamics
   use runge_kutta_integrator, only : DIRK
   
-  type(rigid_body) :: body
   type(DIRK) :: integrator
   type(rigid_body_dynamics), target :: falcon
-  real(dp) :: q(12)=0.0d0, qdot(12)=0.0d0, qddot(12)=0.0d0
-  
-!!$
-!!$  v % x = 2.0d0
-!!$  w % x = 3.0d0
-!!$
-!!$  A = matrix((/ 1.0d0, 2.0d0, 3.0d0, 4.0d0, 5.0d0, 6.0d0, 7.0d0, 8.0d0, 9.0d0 /))
-!!$
-!!$  ! Initial position in inertial frame (r)
-!!$  q(1:3) = (/ 0.0d0, 0.0d0, 25.0d0 /)
-!!$
-!!$  ! Initial velocity in inertial frame (v)
-!!$  q(7:9) = (/ 1.0d0, 2.0d0, 3.0d0 /)
-!!$
-!!$  ! Initial angular velocity in inertial frame (omega)
-!!$  q(10:12) = (/ -1.0d0, -2.0d0, -3.0d0 /)
-!!$  
-!!$  call body % set (1.0d0, q, qdot, qddot)
-!!$  
-!!$  print *, body % get_residual()
-  
+
   ! Set the physics into the integrator
   integrator % system => falcon
 
   call integrator % initialize(tfinal = 10.0d0, num_stages=3, &
-       & h=0.01d0, nvars=12, second_order=.false.)
+       & h=0.01d0, nvars=12, second_order=.true.)
   call integrator % integrate()
   call integrator % write_solution()
   call integrator % finalize()
