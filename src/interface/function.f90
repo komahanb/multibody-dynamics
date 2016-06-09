dd1b99ad12edf773aefbb5c1a354f21f563784e6
komahanb <komahan.cool@gmail.com>
2016-06-09 09:40:07 -0400

    added a function interface

---
+++ src/interface/function.f90
@@ -0,0 +1,45 @@
+!=====================================================================!
+! Module that contains common procedures for any physical system
+! subject to governing equations
+!
+! Author: Komahan Boopathy (komahan@gatech.edu)
+!=====================================================================!
+
+module function_class
+  
+  !-------------------------------------------------------------------!
+  ! Type that models any funtion of interest e.g. kinetic energy, lift
+  !-------------------------------------------------------------------!
+  
+  type, abstract :: function
+
+   contains
+
+     procedure(interface_evaluate), deferred :: getValue    ! function value at t, X, U, Udot, Uddot
+     procedure(interface_evaluate), deferred :: getdFdX     ! partial derivative
+     procedure(interface_evaluate), deferred :: getdFdU     ! partial derivative
+     procedure(interface_evaluate), deferred :: getdFdUDot  ! partial derivative
+     procedure(interface_evaluate), deferred :: getdFdUDDot ! partial derivative
+
+  end type function
+
+  interface
+
+     !----------------------------------------------------------------!
+     ! Interface for evaluting the function for t, X, U, Udot, Uddot
+     !----------------------------------------------------------------!
+
+     subroutine interface_evaluate(this, res, time, x, u, udot, uddot)
+
+       import function
+
+       class(function) :: this
+       real(8), intent(inout), dimension(:) :: res
+       real(8), intent(in) :: time
+       real(8), intent(in), dimension(:) :: x, u, udot, uddot
+       
+     end subroutine interface_evaluate
+
+  end interface
+
+end module physics_class
